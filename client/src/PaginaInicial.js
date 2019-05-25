d724aed527ee82af8b1220e278a021ee0980616c
Andre Santos <santos.as96@hotmail.com>
2019-05-25 02:47:31 +0100

    Adição do navbar responsivo

--- client/src/PaginaInicial.js
+++ client/src/PaginaInicial.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import './PaginaInicial.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Carousel from 'react-bootstrap/Carousel'
 
 function PaginaInicial() {
   return (
-    <p>Pagina Inicial</p>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">ASAF Enterprise</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Acerca de Nós</Nav.Link>
+          <Nav.Link href="#features">Oportunidades de Emprego</Nav.Link>
+          <Nav.Link href="#pricing">Contactos</Nav.Link>
+        </Nav>
+        <Form inline>
+        <FormControl type="text" placeholder="Procure aqui..." className="mr-sm-2" />
+        <Button variant="outline-info">Pesquisar</Button>
+      </Form>
+      </Navbar.Collapse>
+  </Navbar>
   );
 }
 
