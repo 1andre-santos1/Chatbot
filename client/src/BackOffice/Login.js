55ce5a8d5415b5769c97d3ce4c1937bb37656409
Andre Santos <santos.as96@hotmail.com>
2019-05-30 18:57:30 +0100

    template de pag login finalizado

--- client/src/BackOffice/Login.js
+++ client/src/BackOffice/Login.js
@@ -1,8 +1,18 @@
 import React, { Component }  from 'react';
+import './Login.css'
 
 function Login(){
     return(
-        <h3>Login</h3>
+        <div id="LoginContainer">
+            <h3>Login</h3>
+            <form>
+                <label class="LoginLabel">Username</label>
+                <input type="text" name="username" class="LoginInput"/>
+                <label class="LoginLabel">Password</label>
+                <input type="password" name="password"  class="LoginInput"/>
+                <input type="submit" value="Login" class="LoginButton"/>
+            </form>
+        </div>
     );
 }
 
